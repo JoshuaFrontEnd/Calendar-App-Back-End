16b26a2dc1f7e76f3ea50578714d88324995f653
Joshua <hola@joshua.cl>
2023-12-18 12:57:18 -0300

    Validacion de formulario con 'express-validator'

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 // Para no perder intellisense declarar nuevamente express y agregar el valor por defecto "response" a "res" de las funcines
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 const crearUsuario = ( req, res = response ) => {
 
@@ -8,11 +9,23 @@ const crearUsuario = ( req, res = response ) => {
   // 400 es el "status code"
   // Para mas informacion: https://www.restapitutorial.com/httpstatuscodes.html
   // Siempre hacer un "return" al retornar una respuesta, ya que si no se hace, va a retornar la respuesta del if y la de mas abajo, basicamente siempre se debe retornar una sola respuesta o express en la consola dara error
-  if ( name.length < 5 ) {
+  // if ( name.length < 5 ) {
+  //   return res.status( 400 ).json({
+  //     ok: false,
+  //     msg: 'El nombre debe de tener minimo 5 letras'
+  //   })
+  // }
+
+  // Manejo de errores con "express-validator"
+  const errors = validationResult( req );
+
+  if ( !errors.isEmpty() ) {
+
     return res.status( 400 ).json({
       ok: false,
-      msg: 'El nombre debe de tener minimo 5 letras'
+      errors: errors.mapped()
     })
+
   }
 
   res.json({
@@ -27,7 +40,27 @@ const crearUsuario = ( req, res = response ) => {
 
 const loginUsuario = ( req, res = response ) => {
 
+  const errors = validationResult( req );
+
+  if ( !errors.isEmpty() ) {
+
+    return res.status( 400 ).json({
+      ok: false,
+      errors: errors.mapped()
+    })
+
+  }
+
   const { email, password } = req.body;
+
+  if ( !errors.isEmpty() ) {
+
+    return res.status( 400 ).json({
+      ok: false,
+      errors: errors.mapped()
+    })
+
+  }
 
   res.json({
     ok: true,
