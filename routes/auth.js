b3969b8133378bce26585943f1224a9df1d72255
Joshua <hola@joshua.cl>
2023-12-20 14:23:38 -0300

    Validando campos al crear un evento

--- routes/auth.js
+++ routes/auth.js
@@ -7,8 +7,10 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
+
 const { validarCampos } = require('../middlewares/validar-campos');
 const { validarJWT } = require('../middlewares/validar-jwt');
+
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
 
 const router = Router();
--- routes/events.js
+++ routes/events.js
@@ -6,7 +6,13 @@
 ---------------------------------------------------------------- */
 
 const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { isDate } = require('../helpers/isDate');
+
+const { validarCampos } = require('../middlewares/validar-campos');
 const { validarJWT } = require('../middlewares/validar-jwt');
+
 const { createEvent, readEvent, updateEvent, deleteEvent } = require('../controllers/events');
 
 const router = Router();
@@ -15,7 +21,15 @@ const router = Router();
 router.use( validarJWT );
 
 // Create
-router.post('/', createEvent );
+router.post(
+  '/',
+  [
+    check('title','El titulo es obligatorio').not().isEmpty(),
+    check('start','Fecha de inicio es obligatoria').custom( isDate ),
+    check('end','Fecha de finalizacion es obligatoria').custom( isDate ),
+    validarCampos
+  ],
+  createEvent );
 
 // Read
 router.get('/', readEvent );
