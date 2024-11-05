dad563ab17756e57d6c6e707d53a0e7f930df826
Joshua <hola@joshua.cl>
2024-11-04 23:22:00 -0300

    Validación de longitud de contraseña

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,8 +4,7 @@ const bcrypt = require('bcryptjs');
 const Usuario = require('../models/Usuario');
 const { generarJWT } = require('../helpers/jwt');
 
-const crearUsuario = async ( req, res = response ) => {
-
+const crearUsuario = async (req, res = response) => {
   // 400 es el "status code"
   // Para mas informacion: https://www.restapitutorial.com/httpstatuscodes.html
   // Siempre hacer un "return" al retornar una respuesta, ya que si no se hace, va a retornar la respuesta del if y la de mas abajo, basicamente siempre se debe retornar una sola respuesta o express en la consola dara error
@@ -19,119 +18,104 @@ const crearUsuario = async ( req, res = response ) => {
   const { email, password } = req.body;
 
   try {
-
     // Validando si existe el correo
 
     // let usuario = Usuario.findOne({ email: email});
     let usuario = await Usuario.findOne({ email });
 
-    if ( usuario ) {
-      return res.status( 400 ).json({
+    if (usuario) {
+      return res.status(400).json({
         ok: false,
-        msg: 'Ya existe un usuario con ese correo'
-      })
+        msg: 'Ya existe un usuario con ese correo',
+      });
     }
 
-    usuario = new Usuario( req.body );
+    usuario = new Usuario(req.body);
 
     // Encriptar contraseña
     const salt = bcrypt.genSaltSync();
 
-    usuario.password = bcrypt.hashSync( password, salt );
+    usuario.password = bcrypt.hashSync(password, salt);
 
     await usuario.save();
 
     // Generar JWT
-    const token = await generarJWT( usuario.id, usuario.name );
+    const token = await generarJWT(usuario.id, usuario.name);
 
-    res.status( 201 ).json({
+    res.status(201).json({
       ok: true,
       uid: usuario.id,
       name: usuario.name,
-      token
-    })
+      token,
+    });
+  } catch (error) {
+    console.log(error);
 
-  } catch ( error ) {
-
-    console.log( error );
-
-    res.status( 500 ).json({
+    res.status(500).json({
       ok: false,
-      msg: 'Por favor hable con el administrador'
-    })
-
+      msg: 'Por favor hable con el administrador',
+    });
   }
+};
 
-
-
-}
-
-const loginUsuario = async ( req, res = response ) => {
-
+const loginUsuario = async (req, res = response) => {
   const { email, password } = req.body;
 
   try {
-
     const usuario = await Usuario.findOne({ email });
 
-    if ( !usuario ) {
-      return res.status( 400 ).json({
+    if (!usuario) {
+      return res.status(400).json({
         ok: false,
-        msg: 'No existe un usuario con ese correo'
-      })
+        msg: 'No existe un usuario con ese correo',
+      });
     }
 
     // Confirmar los passwords
-    const validPassword = bcrypt.compareSync( password, usuario.password );
+    const validPassword = bcrypt.compareSync(password, usuario.password);
 
-    if ( !validPassword ) {
-      return res.status( 400 ).json({
+    if (!validPassword) {
+      return res.status(400).json({
         ok: false,
-        msg: 'Password incorrecto'
-      })
+        msg: 'Password incorrecto',
+      });
     }
 
     // Generar JWT
-    const token = await generarJWT( usuario.id, usuario.name );
+    const token = await generarJWT(usuario.id, usuario.name);
 
     res.json({
       ok: true,
       uid: usuario.id,
       name: usuario.name,
-      token
-    })
+      token,
+    });
+  } catch (error) {
+    console.log(error);
 
-  } catch ( error ) {
-
-    console.log( error );
-
-    res.status( 500 ).json({
+    res.status(500).json({
       ok: false,
-      msg: 'Por favor hable con el administrador'
-    })
-
+      msg: 'Por favor hable con el administrador',
+    });
   }
+};
 
-}
-
-const revalidarToken = async ( req, res = response ) => {
-
+const revalidarToken = async (req, res = response) => {
   const { uid, name } = req;
 
   // Generando nuevo token
-  const token = await generarJWT( uid, name );
+  const token = await generarJWT(uid, name);
 
   res.json({
     ok: true,
     uid,
     name,
-    token
-  })
-
-}
+    token,
+  });
+};
 
 module.exports = {
   crearUsuario,
   loginUsuario,
-  revalidarToken
-}
+  revalidarToken,
+};
